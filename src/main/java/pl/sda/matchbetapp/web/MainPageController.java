4fa3e49dd33938036a9eedb9d36d618ee8ba6e14
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-07-06 19:27:30 +0200

    spring mvc - first dynamic page

--- src/main/java/pl/sda/matchbetapp/web/MainPageController.java
+++ src/main/java/pl/sda/matchbetapp/web/MainPageController.java
@@ -1,15 +1,30 @@
 package pl.sda.matchbetapp.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 @Controller
 public class MainPageController {
 
     @GetMapping("/")
-    public String displayMainPage() {
+    public String displayMainPage(ModelMap modelMap) {
+        modelMap.addAttribute("currentDate", LocalDate.now());
         return "main";
     }
+
+  /*Druga wersja - zrobi to samo
+  uzycie jednej z nich jest wedlug
+
+    @GetMapping("/")
+    public ModelAndView displayMainPage() {
+        ModelAndView mav = new ModelAndView("main");
+        mav.addObject("currentDate", LocalDate.now());
+        return mav;
+    }*/
 }
 
 
