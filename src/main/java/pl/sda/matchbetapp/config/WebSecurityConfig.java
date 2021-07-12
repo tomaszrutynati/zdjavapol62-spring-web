913f2ffc91384dd1f09aafd365cb543116e32f43
Tomasz Rutyna <tomasz.rutyna@altkom.pl>
2021-07-12 18:45:02 +0200

    spring security - in memory authentication

--- src/main/java/pl/sda/matchbetapp/config/WebSecurityConfig.java
+++ src/main/java/pl/sda/matchbetapp/config/WebSecurityConfig.java
@@ -1,19 +1,37 @@
 package pl.sda.matchbetapp.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("test").roles("ADMIN")
+                .and().withUser("user").password("user").roles("USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/match/all").permitAll()
-                .and().authorizeRequests().antMatchers("/api/**", "/match", "/match/details*",
-                    "/match/edit*", "/user/**", "/bet/**").authenticated()
+                .and().authorizeRequests().antMatchers("/api/**", "/match/details*",
+                    "/user/**", "/bet/**").hasRole("USER")
+                .and().authorizeRequests().antMatchers("/match", "/match/details*",
+                    "/match/edit*", "/match/delete*", "/user/**").hasRole("ADMIN")
                 .and().formLogin()
                 .and().logout()
                 .and().csrf().disable();
